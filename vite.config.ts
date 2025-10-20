82d5bd08d9587cb4610422d343bedf0c32edf20a
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-10-20 20:20:23 +0900

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+// declare module "@remix-run/node" {
+//   interface Future {
+//     v3_singleFetch: true;
+//   }
+// }
 
 export default defineConfig({
   plugins: [
@@ -15,8 +15,9 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-        v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+        v3_singleFetch: false,
+        unstable_optimizeDeps: true,
       },
     }),
     tsconfigPaths(),
